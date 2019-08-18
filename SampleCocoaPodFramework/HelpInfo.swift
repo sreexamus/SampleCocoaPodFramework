ab326f946e71d0ecea0f90798c4aa2148ffdcb0a
sreexamus <sreestarta.com>
2019-08-18 16:53:06 -0500

    added files
    added files

---
+++ SampleCocoaPodFramework/HelpInfo.swift
@@ -0,0 +1,15 @@
+//
+//  HelpInfo.swift
+//  SampleCocoaPodFramework
+//
+//  Created by sreekanth reddy iragam reddy on 8/18/19.
+//  Copyright © 2019 CocoaPods Framework. All rights reserved.
+//
+
+import Foundation
+
+public class HelpInfo {
+    static func getInfo() -> String {
+        return "Welcome"
+    }
+}
